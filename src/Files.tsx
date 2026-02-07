3e67146a065b92eb7d923bca7559a845f09230ad
Luc de Wit <lucdewit@Lucs-MacBook-Pro-2.local>
2026-02-07 14:01:56 +0100

    Feature: Abbreviate current directory path using [home]

--- src/Files.tsx
+++ src/Files.tsx
@@ -2,6 +2,7 @@ import fs from 'fs'
 import settings from "./settings"
 import { useFileStore } from "./store"
 import { useEffect, useRef } from "react"
+import os from "os"
 
 export function Files() {
     const scrollRef = useRef<any>(null);
@@ -9,6 +10,15 @@ export function Files() {
     const setIndex = useFileStore((state: any) => state.setIndex);
     const directory = useFileStore((state: any) => state.directory);
     const files = useFileStore((state: any) => state.files) as Array<{ name: string, indx: number, type: "dir" | "file" | "unknown" }>;
+
+    // Transform directory path
+    const homedir = os.homedir();
+    const transformDirectoryPath = (path: string) => {
+        if (path.startsWith(homedir)) {
+            return path.replace(homedir, "[home]");
+        }
+        return path;
+    };
 
     const type2iconMap = {
         "dir": "📂",
@@ -23,7 +33,7 @@ export function Files() {
     }, [cursorIndex]);
 
     return (
-        <scrollbox focused={true} ref={scrollRef} title={directory} id='files' scrollY borderColor={settings.border.color.dimmed} focusedBorderColor={settings.border.color.focus} borderStyle="rounded" flexDirection="row" height="100%" width="100%" paddingRight={0}>
+        <scrollbox focused={true} ref={scrollRef} title={transformDirectoryPath(directory)} id='files' scrollY borderColor={settings.border.color.dimmed} focusedBorderColor={settings.border.color.focus} borderStyle="rounded" flexDirection="row" height="100%" width="100%" paddingRight={0}>
             {files.map((file, idx) => (
                 <text fg={file.indx == cursorIndex ? settings.border.color.bright : settings.border.color.dimmed}>
                     {type2iconMap[file.type]} {file.name}
